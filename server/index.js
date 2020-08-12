30df1e61a473e2245aea060d6647f20179b8aa01
smcfadden150 <smcfadden150@gmail.com>
2020-08-12 02:05:01 -0400

    Added Axios Support to Vue Client, Added Configs for Prod/Dev

--- server/index.js
+++ server/index.js
@@ -6,13 +6,26 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.DEV_PORT;
+const port = process.env.port;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded( { extended: true } ));
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+}
+
 app.get('/', (req, res) => {
     console.log("Poggers you got an API request");
+});
+
+app.get('/test', (req, res) => {
+    console.log("Got a request from the vue app! nice!");
+    res.send("Welcome to 97 Degree Cider!");
 });
 
 app.listen(port);
